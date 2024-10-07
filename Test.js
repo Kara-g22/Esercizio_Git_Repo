8eb10f97b44b9d8cb3b3adb232c37625d37af4a8
Marco Grassi <marco.grassi.private@gmail.com>
2024-10-08 01:04:19 +0200

    Copia esercizio Develhope

--- Test.js
+++ Test.js
@@ -1,8 +1,20 @@
-let nome="Marco ";
-let età= 24;
-const sesso= " Maschio";
+let rettangoloHeight= 2;
+let rettangoloWidth= 1;
 
-let anagrafica= nome + età + sesso
+let radiusCircle= 3;
 
+let lasagnaCost= 24
+let pizzaCost= 12
+let toastCost= 5
 
-console.log(anagrafica)
+console.log(rettangoloHeight);
+console.log(rettangoloWidth);
+
+console.log(radiusCircle);
+console.log(lasagnaCost);
+console.log(pizzaCost);
+console.log(toastCost);
+
+let sum= (3+6)*2
+
+console.log(sum)
