3f8e4cc168e045c6857b77c9b166edb17dc0269b
Marco Grassi <marco.grassi.private@gmail.com>
2024-10-10 02:58:36 +0200

    Esercizio java giorno 3

---
+++ test3.js
@@ -0,0 +1,40 @@
+    //ESERCIZIO 1 IF/ELSE
+
+// let name;
+// let surname= "Rossi";
+// let fullName= (name + " " + surname);
+
+// if(name===true && surname===true){
+//     console.log("Il tuo nome è"+ " " +fullName);
+// }else{
+//     console.log("Fullname is invalid");
+// }
+
+    //ESERCIZIO 2 IF/ELSE
+
+// let pin= 1000;
+
+// if(pin>999 && pin<=10000){
+//     console.log("Pin is valid");
+// }else{
+//     console.log("pin is not valid")
+// }
+
+    //ESERCIZIO 3 IF/ELSE
+
+let primitive= false;
+
+if(typeof primitive === 'number'){
+    console.log("La variabile è di tipo:");
+    console.log(typeof primitive);
+}else if(typeof primitive === 'string'){
+    console.log("La variabile è di tipo:");
+    console.log(typeof primitive);
+}else if(typeof primitive === 'boolean'){
+    console.log("La variabile è di tipo:");
+    console.log(typeof primitive);
+}else{
+    console.log("La variabile è di tipo sconosciuto")
+}
+
+console.log("Done");
