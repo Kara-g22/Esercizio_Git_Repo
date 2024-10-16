30fbe09ee83ce3b702c415b9ceeb890aafbbefce
Marco Grassi <marco.grassi.private@gmail.com>
2024-10-16 12:29:33 +0200

    commit

--- Es(2).js
+++ Es(2).js
@@ -15,19 +15,21 @@
 // sayHelloName("Marco","Francesco",printName, "Ciao");
 
 
-let sum=(a,b)=> a+b;
-let div=(a,b)=> a/b;
-let multi=(a,b)=> a*b;
-let diff=(a,b)=> a-b;
+// let sum=(a,b)=> a+b;
+// let div=(a,b)=> a/b;
+// let multi=(a,b)=> a*b;
+// let diff=(a,b)=> a-b;
 
-function operazioni(a,b,c,d,e,f,g,h) {
-    if (a && b && c && d && typeof a==="number" && typeof b==="number" && typeof c==="number" && typeof d==="number") {
-        return (e(a,b)*f(a,b)+(g(a,b)-h(a,b)))*d
-    }else{
-        console.log("Errore")
-    }
+// function operazioni(a,b,c,d,e,f,g,h) {
+//     if (a && b && c && d && typeof a==="number" && typeof b==="number" && typeof c==="number" && typeof d==="number") {
+//         return (e(a,b)*f(a,b)+(g(a,b)-h(a,b)))*d
+//     }else{
+//         console.log("Errore")
+//     }
     
 
-}
+// }
 
-console.log(operazioni(10,9,8,7,multi,sum,diff,div));
+// console.log(operazioni(10,9,8,7,multi,sum,diff,div));
+
+
