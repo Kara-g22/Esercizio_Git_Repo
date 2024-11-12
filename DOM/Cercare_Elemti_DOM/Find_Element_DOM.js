eb3b209f7af4bf555074a66622dc90f549e4c9db
Marco Grassi <marco.grassi.private@gmail.com>
2024-11-12 17:41:17 +0100

    find element DOM

---
+++ DOM/Cercare_Elemti_DOM/Find_Element_DOM.js
@@ -0,0 +1,8 @@
+const p = document.getElementsByClassName("paragrafo")
+console.log(p);
+const element = document.getElementById("elemento-3")
+console.log(element);
+const h2= document.querySelectorAll("h2")
+console.log(h2);
+
+
